6856926675f65e52f5ac159d5807dd6f15a8f3de
LTancock <l.j.tancock@gmail.com>
2025-04-07 20:44:37 +0100

    Added in some of the transformations, not sure how many I'll do yet

--- generate/Generate_MNIST.py
+++ generate/Generate_MNIST.py
@@ -1,7 +1,42 @@
 import numpy as np
 from os.path  import join
+import scipy.ndimage
 from Read_MNIST import MnistDataloader
 import matplotlib.pyplot as plt
+import scipy
+import random
+
+
+def add_gaussian_noise(sample, mean=0, std=10):
+    noise = np.random.normal(loc=mean, scale=std, size=(len(sample), len(sample[0])))
+    noisyImage = sample + noise
+    # Clip values to keep them in valid range [0, 255]
+    noisyImage = np.clip(noisyImage, 0, 255)
+    return noisyImage
+
+#Transformations
+#Focussing on transformations that are not easily corrected for, as it isn't as difficult to fix those in the testing stage
+
+#Rotation
+def rotateMNIST(sample, noRotations, angle):
+    rotated = []
+    for x in range (0, noRotations):
+        rotated.append(scipy.ndimage.rotate(sample, angle=random.randint(-angle, angle), reshape=False, mode='constant', cval=0.0))
+    return rotated
+
+#Gaussian blur
+def blurMNIST(sample, noBlurs):
+    blurred = []
+    for x in range(0, noBlurs):
+        blurred.append(scipy.ndimage.gaussian_filter(sample, sigma=random.uniform(0,1)))
+    return blurred
+
+#Gaussian noise
+def noisyMNIST(sample, noNoise, deviation):
+    noised = []
+    for x in range(0, noNoise):
+        noised.append(add_gaussian_noise(sample, std=deviation))
+    return noised
 
 
 input_path = 'input'
@@ -15,15 +50,14 @@ mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_fil
 
 #ABOVE IS NOT MY CODE, copied from Read_MNIST
 
-#number of samples to pull from training set
+#number of samples to pull of each digit from training set
 noSamples = 1
 # Create a 1D NumPy array of empty lists, treating each element as an object
 samples = np.empty(10, dtype=object)
 
-# Initialize each element as an empty list
+# Initialize each element as an empty list, creating one for each digit
 for i in range(10):
     samples[i] = []
-
 
 #Don't know the ordering of MNIST data so just searching through linearly for each thing, might change this later
 #y_train[x] is the number presented in the image in position x
@@ -34,8 +68,34 @@ for i in range(0, len(x_train)):
         
 
 
-plt.figure(figsize=(30,20))
-for x in samples:
-    #print(x[0])
-    plt.imshow(x[0])
+#Showing the samples
+#plt.figure(figsize=(30,20))
+#for x in samples:
+    #plt.imshow(x[0])
+    #plt.show()
+plt.imshow(samples[0][0])
+plt.show()
+#print(samples[0])
+#print("\n\n\n\n\n\n")
+#print(rotateMNIST(samples[0][0]))
+
+#Rotations
+#rotations = rotateMNIST(samples[0][0], 5, 15)
+#for x in rotations:
+#    plt.imshow(x)
+#    plt.show()
+
+#Gaussian Blur
+#Effect is subdued on low pixel density images, not sure that it will do much useful/might cause overfitting
+#blurred = blurMNIST(samples[0][0], 5)
+#for x in blurred:
+#    plt.imshow(x)
+#    plt.show()
+
+#Gaussian Noise
+#Not sure how much to put on the deviation, will try out a few but probably quite small
+#20 is the around the amount you'd expect, 50 is relatively bad quality
+noised = noisyMNIST(samples[0][0], 5, 20)
+for x in noised:
+    plt.imshow(x)
     plt.show()
